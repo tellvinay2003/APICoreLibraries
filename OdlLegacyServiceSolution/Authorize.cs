df685ba8d021815c8bfe38058e45d1942b030264
vinay pandey <tellvinay2003@yahoo.co.in>
2020-05-25 14:25:01 +0530

    AuthAPI added

---
+++ OdlLegacyServiceSolution/Authorize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdlLegacyServiceSolution
+{
+    public class Authorize: Attribute
+    {
+        public bool IsValid()
+        {
+            return true;
+        }
+    }
+}
---
+++ OdlLegacyServiceSolution/DistributorValidator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace OdlLegacyServiceSolution
+{
+    public class DistributorValidator : ServiceAuthorizationManager
+    {
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            //string auth = OperationContext.Current.IncomingMessageHeaders.GetHeader<string>("Authorization", "https://localhost:44313/GetMappingsValue");
+            //var token = HttpContext.Current.Request.Headers["Token"];
+            //var authHeader = WebOperationContext.Current.IncomingRequest.Headers["Authorization"];
+            //var messageHdr = operationContext.IncomingMessageHeaders;
+
+            string tokenStr = string.Empty;
+
+            OperationContext context = OperationContext.Current;
+            if (context != null && context.RequestContext != null)
+            {
+                System.ServiceModel.Channels.Message msg = context.RequestContext.RequestMessage;
+                tokenStr = msg.ToString();
+            }
+
+            string token = GetTokenValue(tokenStr);
+
+            if (!string.IsNullOrEmpty(token))
+                return true;
+            else
+                return false;
+        }
+
+        private bool IsValid(string strToken)
+        {
+            //TOD: Read token and verify 
+
+            return true;
+        }
+
+        private string GetTokenValue(string envlopeData)
+        {
+            int index = envlopeData.IndexOf("Bearer");
+            envlopeData = envlopeData.Substring(index, envlopeData.Length - index);
+            index = envlopeData.IndexOf("</Token>");
+            envlopeData = envlopeData.Substring(0, index);
+            return envlopeData;
+        }
+    }
+}
